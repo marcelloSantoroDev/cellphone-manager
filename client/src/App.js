b3ddd04a81690d3fb3996923cd4742e4d9416fef
Marcello <marcelloaltermundi@gmail.com>
2024-03-01 11:18:22 -0300

    feat: first layout for signup's frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/signup" component={Signup} />
     </Switch>
   );
 }
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './CSS/Signup.css';
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div className='signup-container'>
+            <form className='form-container' action="">
+                <input className='email-input' type="text" placeholder='Email' />
+                <input className='password-input' type="text" placeholder='Password' />
+                <button>Signup</button>
+            </form>
+        </div>
+    )
 }
 
-export default Signup
+export default Signup;
