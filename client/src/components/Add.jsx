1cd560f1e1a972bf2f9ba81df6d69e751927a7ec
Marcello <marcelloaltermundi@gmail.com>
2024-03-01 12:21:40 -0300

    feat: first layout for /add-products

--- client/src/components/Add.jsx
+++ client/src/components/Add.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './CSS/Add.css';
 
 function Add() {
   return (
-    <div>Add</div>
+    <div className='add-product-container'>
+      <form className='add-product-form' action="">
+        <input type="text" placeholder='Name' />
+        <input type="text" placeholder='Brand' />
+        <input type="text" placeholder='Model' />
+        <input type="text" placeholder='Price' />
+        <input type="text" placeholder='Color' />
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
