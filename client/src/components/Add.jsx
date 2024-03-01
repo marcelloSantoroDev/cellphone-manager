42673bb87be688d3749e7ebd513cf1d54c622c11
Marcello <marcelloaltermundi@gmail.com>
2024-03-01 13:23:26 -0300

    adds event listener on /add-product

--- client/src/components/Add.jsx
+++ client/src/components/Add.jsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './CSS/Add.css';
+import AppContext from '../context/AppContext';
 
 function Add() {
+  const {addProduct, setAddProduct} = useContext(AppContext);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setAddProduct({...addProduct, [name]: value});
+  }
   return (
     <div className='add-product-container'>
       <form className='add-product-form' action="">
-        <input type="text" placeholder='Name' />
-        <input type="text" placeholder='Brand' />
-        <input type="text" placeholder='Model' />
-        <input type="text" placeholder='Price' />
-        <input type="text" placeholder='Color' />
+        <input
+        value={addProduct.name}
+        name='name'
+        type="text"
+        placeholder='Name'
+        onChange={handleChange}
+        />
+        <input
+        value={addProduct.brand}
+        name='brand'
+        type="text"
+        placeholder='Brand'
+        onChange={handleChange}
+        />
+        <input
+        value={addProduct.model}
+        name='model'
+        type="text"
+        placeholder='Model'
+        onChange={handleChange}
+        />
+        <input
+        value={addProduct.price}
+        name='price'
+        type="text"
+        placeholder='Price'
+        onChange={handleChange}
+        />
+        <input
+        value={addProduct.color}
+        name='color'
+        type="text"
+        placeholder='Color'
+        onChange={handleChange}
+        />
         <button>Submit</button>
       </form>
     </div>
