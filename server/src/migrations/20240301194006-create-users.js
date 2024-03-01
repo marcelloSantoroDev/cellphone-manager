7ba8ab2a35331d4f160d45e601d2659f3e58716e
Marcello <marcelloaltermundi@gmail.com>
2024-03-01 16:48:35 -0300

    feat: creates migration and model for users

--- server/src/migrations/20240301194006-create-users.js
+++ server/src/migrations/20240301194006-create-users.js
@@ -3,12 +3,25 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
