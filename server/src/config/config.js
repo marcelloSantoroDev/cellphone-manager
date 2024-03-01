f61a868990c256c452a414263101eea3c00748c1
Marcello <marcelloaltermundi@gmail.com>
2024-03-01 18:34:18 -0300

    errors in db connection

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
+    username: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
