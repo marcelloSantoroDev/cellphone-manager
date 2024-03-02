d1d2ba63e9b48dd023ba2fd1e1793cc751b09feb
Marcello <marcelloaltermundi@gmail.com>
2024-03-02 10:30:13 -0300

    fix: db connection

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || "root",
+    username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
